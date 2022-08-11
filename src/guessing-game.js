68e752ae377754e8bf295b6042eb611cc8ab3c33
DashaErmolich <dasha.ermolich@gmail.com>
2022-08-11 10:41:13 +0200

    feat: solved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+            this.medium = Math.ceil((this.min + this.max)/2);
+            return this.medium
     }
 
     lower() {
-
+        this.max = this.medium;
+        this.guess()
     }
 
     greater() {
-
+        this.min = this.medium;
+        this.guess()
     }
 }
 
